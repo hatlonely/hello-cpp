ae8120d83703f47be5fc85284c8d24c77bbeadae
hatlonely <hatlonely@gmail.com>
2024-11-20 03:17:32 +0000

    update

--- test/std/test_future.cpp
+++ test/std/test_future.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <future>
+#include <thread>
 
 namespace testing::future {
 
@@ -23,6 +24,16 @@ TEST(FutureTest, SharedFuture) {
 
     t1.join();
     t2.join();
+}
+
+TEST(FutureTest, Async) {
+    auto future = std::async(std::launch::async, [] {
+        std::cout << "async run in thread " << std::this_thread::get_id() << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        return 10;
+    });
+    std::cout << "main run in thread " << std::this_thread::get_id() << std::endl;
+    EXPECT_EQ(future.get(), 10);
 }
 
 }  // namespace testing::future

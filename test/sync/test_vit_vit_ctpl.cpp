43d8fbb9e31c0d2c695a4f2ee88aeb36cc5990c3
hatlonely <hatlonely@gmail.com>
2024-11-22 05:44:24 +0000

    update

---
+++ test/sync/test_vit_vit_ctpl.cpp
@@ -0,0 +1,33 @@
+#include <ctpl_stl.h>
+#include <gtest/gtest.h>
+
+namespace testing::vit_vit_ctpl {
+
+TEST(VitVitCtpl, ThreadPool) {
+    ctpl::thread_pool pool(4);
+
+    pool.push([](int id) {
+        std::cout << "hello from " << id << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        std::cout << "world from " << id << std::endl;
+    });
+    pool.push(
+        [](int id, int arg1, int arg2) {
+            std::cout << "hello from " << id << " with args " << arg1 << " " << arg2 << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            std::cout << "world from " << id << " with args " << arg1 << " " << arg2 << std::endl;
+        },
+        123, 456
+    );
+    auto future = pool.push([](int id) {
+        std::cout << "hello from " << id << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "world from " << id << std::endl;
+        return 123456;
+    });
+    EXPECT_EQ(future.get(), 123456);
+
+    pool.stop(true);  // 析构的时候也会自动调用
+}
+
+}  // namespace testing::vit_vit_ctpl
